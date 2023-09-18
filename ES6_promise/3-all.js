2a56123c08482c7e14c30d4a55c097bc1130062e
Dominick Keeling <dominickckeeling@gmail.com>
2023-09-18 13:30:46 +0000

    fixed task  4?

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export  default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const [photoResponse, userResponse] = values;
       console.log(`${values[0].body ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
