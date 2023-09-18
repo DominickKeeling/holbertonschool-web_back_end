055db2b8da16919b2c693ca0bf9e7cefb345f33f
Dominick Keeling <dominickckeeling@gmail.com>
2023-09-18 13:26:28 +0000

    fixed task 1 and 2

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export  default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const [photoResponse, userResponse] = values;
-      console.log(`firstName ${userResponse.body} lastName ${photoResponse.body}`);
+      console.log(`${values[0].body ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
